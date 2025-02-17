6db338c12be4f6c1e66263cdc3e0123e0100eafe
rasheed <rasheedoduola007@gamil.com>
2025-02-17 11:49:21 +0100

    Set DEBUG to False for production readiness

--- diarypro/settings.py
+++ diarypro/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ltgel8uo8c880m)bw-ve@lo1l_y18d+gzu1mavr!c2714wvf$v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
