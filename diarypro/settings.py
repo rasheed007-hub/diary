07e80936b7af273ba843233e7d1f9c5b52538163
rasheed <rasheedoduola007@gamil.com>
2025-02-19 13:55:48 +0100

    Update DEBUG setting to True and modify ALLOWED_HOSTS for local development

--- diarypro/settings.py
+++ diarypro/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ltgel8uo8c880m)bw-ve@lo1l_y18d+gzu1mavr!c2714wvf$v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','https://diary-a84s.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1','https://diary-a84s.onrender.com','diary-a84s.onrender.com']
 
 
 # Application definition
